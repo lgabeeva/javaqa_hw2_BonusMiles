26c56ebe8c0e44088ec930b431f8c0e3de53112b
neto test <liubov.gabeeva@mail.ru>
2023-10-08 16:46:08 +0300

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+         int amount = 12_000;
+
+         int extramiles = amount /20;
+
+        System.out.println("Бонусные_мили: " + extramiles );
+    }
+}
